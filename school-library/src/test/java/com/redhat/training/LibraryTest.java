5324846e5c48787adfb376ebeb66336a2d0d17fb
ishida.junichiro <ishida.junichiro@nttcom.co.jp>
2024-03-07 00:55:12 -0500

    Refactor

--- school-library/src/test/java/com/redhat/training/LibraryTest.java
+++ school-library/src/test/java/com/redhat/training/LibraryTest.java
@@ -23,5 +23,24 @@ public class LibraryTest {
         library = new Library(inventory);
     }
 
-    // Add tests here...
+    @Test
+    public void checkingOutUnavailableBookThrowsException()
+        throws BookNotAvailableException {
+    // Given
+    inventory.add(new Book("book1"));
+    inventory.add(new Book("book1"));
+
+    library.checkOut("student1", "book1");
+    library.checkOut("student2", "book1");
+
+    // When
+    final BookNotAvailableException exception = assertThrows(        BookNotAvailableException.class,
+        () -> {
+            library.checkOut("student3", "book1");
+        }
+    );
+
+    // Then
+    assertTrue(exception.getMessage().matches("Book book1 is not available"));
+    }
 }
--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -1,10 +1,20 @@
 package com.redhat.shipping;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class ShippingCalculatorTest {
 
     // @todo: add tests
+    @Test
+        public void canCalculateTheCostForARegion() {
+        ShippingCalculator calculator = new ShippingCalculator();
+
+        assertEquals(0, calculator.costForRegion("A Region"));
+    }
+    @Test
+        public void onNARegionTheCostIs100() {
+        assertEquals(100, (new ShippingCalculator()).costForRegion("NA"));
+    }
 }
